55d7dbb775f6acd24e0746b2397aea9402c97a15
unknown <leonardo01032003@gmail.com>
2024-11-09 04:08:26 +0700

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import Img1 from "../assets/pics/baruu1.jpg";
+import Img2 from "../assets/pics/baruu2.jpg";
+import Img3 from "../assets/pics/baruu3.jpg";
+import Img4 from "../assets/pics/baruu4.jpg";
+import Img5 from "../assets/pics/baruu5.jpg";
+import Img6 from "../assets/pics/baruu6.jpg";
+import Img7 from "../assets/pics/baruu8.jpg";
+import Img8 from "../assets/pics/baruu7.jpg";
+import Img9 from "../assets/pics/baruu10.jpg";
+import Img10 from "../assets/pics/baruu11.jpg";
+import Img11 from "../assets/pics/baruu12.jpg";
+import Img12 from "../assets/pics/baruu13.jpg";
+
+export const images = [
+  {
+    src: Img1,
+  },
+  {
+    src: Img2,
+  },
+  {
+    src: Img3,
+  },
+  {
+    src: Img4,
+  },
+  {
+    src: Img5,
+  },
+  {
+    src: Img6,
+  },
+  {
+    src: Img7,
+  },
+  {
+    src: Img8,
+  },
+  {
+    src: Img9,
+  },
+  {
+    src: Img10,
+  },
+  {
+    src: Img11,
+  },
+  {
+    src: Img12,
+  },
+];
